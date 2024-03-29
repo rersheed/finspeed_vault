8ff763a002093564d2a4784b2aa0e6ed4fa5141d
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-03-29 16:43:28 +0100

    Add UserDataModel class and its serialization methods

---
+++ lib/features/auth/data_layer/models/user_data_model.freezed.dart
@@ -0,0 +1,183 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'user_data_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+UserDataModel _$UserDataModelFromJson(Map<String, dynamic> json) {
+  return _UserDataModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$UserDataModel {
+  String get phoneNumber => throw _privateConstructorUsedError;
+  String get password => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $UserDataModelCopyWith<UserDataModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserDataModelCopyWith<$Res> {
+  factory $UserDataModelCopyWith(
+          UserDataModel value, $Res Function(UserDataModel) then) =
+      _$UserDataModelCopyWithImpl<$Res, UserDataModel>;
+  @useResult
+  $Res call({String phoneNumber, String password});
+}
+
+/// @nodoc
+class _$UserDataModelCopyWithImpl<$Res, $Val extends UserDataModel>
+    implements $UserDataModelCopyWith<$Res> {
+  _$UserDataModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? phoneNumber = null,
+    Object? password = null,
+  }) {
+    return _then(_value.copyWith(
+      phoneNumber: null == phoneNumber
+          ? _value.phoneNumber
+          : phoneNumber // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: null == password
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$UserDataModelImplCopyWith<$Res>
+    implements $UserDataModelCopyWith<$Res> {
+  factory _$$UserDataModelImplCopyWith(
+          _$UserDataModelImpl value, $Res Function(_$UserDataModelImpl) then) =
+      __$$UserDataModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String phoneNumber, String password});
+}
+
+/// @nodoc
+class __$$UserDataModelImplCopyWithImpl<$Res>
+    extends _$UserDataModelCopyWithImpl<$Res, _$UserDataModelImpl>
+    implements _$$UserDataModelImplCopyWith<$Res> {
+  __$$UserDataModelImplCopyWithImpl(
+      _$UserDataModelImpl _value, $Res Function(_$UserDataModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? phoneNumber = null,
+    Object? password = null,
+  }) {
+    return _then(_$UserDataModelImpl(
+      phoneNumber: null == phoneNumber
+          ? _value.phoneNumber
+          : phoneNumber // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: null == password
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$UserDataModelImpl
+    with DiagnosticableTreeMixin
+    implements _UserDataModel {
+  const _$UserDataModelImpl(
+      {required this.phoneNumber, required this.password});
+
+  factory _$UserDataModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$UserDataModelImplFromJson(json);
+
+  @override
+  final String phoneNumber;
+  @override
+  final String password;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'UserDataModel(phoneNumber: $phoneNumber, password: $password)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'UserDataModel'))
+      ..add(DiagnosticsProperty('phoneNumber', phoneNumber))
+      ..add(DiagnosticsProperty('password', password));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$UserDataModelImpl &&
+            (identical(other.phoneNumber, phoneNumber) ||
+                other.phoneNumber == phoneNumber) &&
+            (identical(other.password, password) ||
+                other.password == password));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, phoneNumber, password);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$UserDataModelImplCopyWith<_$UserDataModelImpl> get copyWith =>
+      __$$UserDataModelImplCopyWithImpl<_$UserDataModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$UserDataModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _UserDataModel implements UserDataModel {
+  const factory _UserDataModel(
+      {required final String phoneNumber,
+      required final String password}) = _$UserDataModelImpl;
+
+  factory _UserDataModel.fromJson(Map<String, dynamic> json) =
+      _$UserDataModelImpl.fromJson;
+
+  @override
+  String get phoneNumber;
+  @override
+  String get password;
+  @override
+  @JsonKey(ignore: true)
+  _$$UserDataModelImplCopyWith<_$UserDataModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/features/auth/data_layer/models/user_data_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_data_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserDataModelImpl _$$UserDataModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserDataModelImpl(
+      phoneNumber: json['phoneNumber'] as String,
+      password: json['password'] as String,
+    );
+
+Map<String, dynamic> _$$UserDataModelImplToJson(_$UserDataModelImpl instance) =>
+    <String, dynamic>{
+      'phoneNumber': instance.phoneNumber,
+      'password': instance.password,
+    };
