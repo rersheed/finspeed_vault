c93af962347b999d75fd18babfd9d045a8471775
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-03-30 15:22:06 +0100

    Add router configuration to MaterialApp

---
+++ lib/core/utils/router.dart
@@ -0,0 +1,28 @@
+import 'package:finspeed_vault/features/darshboard/application_layer/screens/dashboard_screen.dart';
+import 'package:finspeed_vault/features/onboarding/application_layer/screens/onboarding_screen.dart';
+import 'package:finspeed_vault/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final _router = GoRouter(
+  routes: [
+    // HomeScreen
+    GoRoute(
+      path: HomeScreen.routeName,
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        //dashboard screen
+        GoRoute(
+          path: DashboardScreen.routeName,
+          builder: (context, state) => const DashboardScreen(),
+        ),
+      ],
+    ),
+    // OnboardingScreen
+    GoRoute(
+        path: OnboardingScreen.routeName,
+        builder: (context, state) => const OnboardingScreen()),
+    //
+  ],
+);
+
+final router = _router; 
